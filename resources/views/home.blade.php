12fd938a636e65fba00e73a7070da0366b391d2a
mubar3 <khusnul.mubar@yahoo.com>
2023-06-18 22:05:03 +0700

    update date report

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -102,8 +102,14 @@
 <script>
     
     // date one month ago
-    var formattedDate = new Date(new Date().getFullYear(), new Date().getMonth() - 1, new Date().getDate()).toISOString().slice(0, 10);
-    $('#tgl_awal').val(formattedDate);
+    // var formattedDate = new Date(new Date().getFullYear(), new Date().getMonth() - 1, new Date().getDate()).toISOString().slice(0, 10);
+    // $('#tgl_awal').val(formattedDate);
+
+    // date in day 1
+    var currentDate = new Date();
+    currentDate.setDate(1);
+    $('#tgl_awal').val(currentDate.toISOString().slice(0, 10));
+
     document.getElementById('tgl_akhir').valueAsDate = new Date();
     
     $('#loading').hide();
